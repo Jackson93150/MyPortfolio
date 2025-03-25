d148970583ce2db590d071d55a2be290a02cac09
Jackson93150 <jackjack93150@gmail.com>
2025-03-26 00:52:44 +0100

    feat: bento grid

--- src/app/sections/skills.js
+++ src/app/sections/skills.js
@@ -1,9 +1,28 @@
 import { motion } from "framer-motion";
 import Image from "next/image";
+import InfiniteScroll from "../ui/infinitescroll";
+import BentoGrid from "../ui/bentogrid";
+
+const items = [
+  { content: "Text Item 1" },
+  { content: <p>Paragraph Item 2</p> },
+  { content: "Text Item 3" },
+  { content: <p>Paragraph Item 4</p> },
+  { content: "Text Item 5" },
+  { content: <p>Paragraph Item 6</p> },
+  { content: "Text Item 7" },
+  { content: <p>Paragraph Item 8</p> },
+  { content: "Text Item 9" },
+  { content: <p>Paragraph Item 10</p> },
+  { content: "Text Item 11" },
+  { content: <p>Paragraph Item 12</p> },
+  { content: "Text Item 13" },
+  { content: <p>Paragraph Item 14</p> },
+];
 
 export default function Skills() {
   return (
-    <div className="relative z-10 flex flex-col md:flex-row h-auto min-h-screen w-full items-center justify-center md:justify-between px-4 md:px-16 py-10 md:py-20">
+    <div className="relative z-10 flex flex-col h-auto min-h-screen w-full items-center gap-16 px-4 md:px-16 py-10 md:py-20">
       <motion.h2
         initial={{ scale: 0 }}
         whileInView={{ scale: 1 }}
@@ -14,10 +33,13 @@ export default function Skills() {
           damping: 10,
           delay: 0.3,
         }}
-        className="z-10 w-[50vw] flex justify-center"
+        className="text-white text-[5vw]"
       >
         My Skills
       </motion.h2>
+      <div className="flex relative w-[80vw] h-[65vh] rounded-lg text-white">
+        <BentoGrid />
+      </div>
     </div>
   );
 }
